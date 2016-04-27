d17e521205a374f070456d754fc6786a72a43501
btmk <mohammed.informatics@gmail.com>
2016-04-27 19:25:06 +0200

    27-4

--- 2DGameProject/Code/Game/InGame/InGameState.cs
+++ 2DGameProject/Code/Game/InGame/InGameState.cs
@@ -11,9 +11,10 @@ namespace GameProject2D
         
         public InGameState()
         {
-            player = new Player(new Vector2f(10F, 10F));
+            player = new Player(new Vector2f(100F, 100F));
+            
         }
-
+       
         public GameState Update(float deltaTime)
         {
             player.update(deltaTime);
@@ -29,9 +30,6 @@ namespace GameProject2D
         {
         }
 
-        public Player getPlayer()
-        {
-            return player;
-        }
+       
     }
 }
