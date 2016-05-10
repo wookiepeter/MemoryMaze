cfb274c84d9212d4099cbcaba052e74a4052e7d5
wookiepeter <matthis.hagen@o2online.de>
2016-05-10 21:17:16 +0200

    gerd hat meinen bug gefixt

--- 2DGameProject/Code/Game/InGame/Map.cs
+++ 2DGameProject/Code/Game/InGame/Map.cs
@@ -25,14 +25,11 @@ namespace GameProject2D
         private Cell[,] randomCellMap(int sizeX, int sizeY)
         {
             Cell[,] newCellArray = new Cell[sizeX, sizeY];
-            Random rand = new Random();
-            int randInt;
             for (int i = 0; i < sizeX; i++)
             {
-                for (int j = 0; i < sizeY; j++)
+                for (int j = 0; j < sizeY; j++)
                 {
-                    randInt = (int)(rand.Next() * 2);
-                    newCellArray[i, j].setContent((cellContent) randInt);
+                    newCellArray[i, j] = new Cell((cellContent)Rand.IntValue(0, 2));
                 }
             }
             return newCellArray;
