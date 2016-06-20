0eb9449d646ab7dadded632b303b6cea664ce67b
12Cy <christian.sandkaemper@gmail.com>
2016-06-20 14:48:21 +0200

    strukturiert von A-Z

---
+++ 2DGameProject/Code/Game/ManageGame/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace MemoryMaze
+{
+    class Game
+    {
+        //simply contains and manage all levels of the game
+        Level level;
+        
+
+        public Game()
+        {
+            level = new Level();
+        }
+
+        public void Update(float deltaTime)
+        {
+            level.update(deltaTime);
+        }
+
+        public void draw(RenderWindow win, View view)
+        {
+            
+            level.draw(win, view);
+        }
+    }
+}
